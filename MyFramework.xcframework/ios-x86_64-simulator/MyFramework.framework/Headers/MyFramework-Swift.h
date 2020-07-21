f6943ea2c7bd2a596256a1b873c26a5f1fb635d2
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2020-07-21 17:01:40 +0700

    NewVC

--- MyFramework.xcframework/ios-x86_64-simulator/MyFramework.framework/Headers/MyFramework-Swift.h
+++ MyFramework.xcframework/ios-x86_64-simulator/MyFramework.framework/Headers/MyFramework-Swift.h
@@ -217,6 +217,13 @@ SWIFT_CLASS("_TtC11MyFramework19LoginViewController")
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil SWIFT_UNAVAILABLE;
 @end
 
+
+SWIFT_CLASS("_TtC11MyFramework5NewVC")
+@interface NewVC : UIViewController
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
+@end
+
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
